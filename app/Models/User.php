4dd713e05e463539ee5c5fa8130a3806f1e53e20
严丹婵 <386000643@qq.com>
2018-10-10 05:13:19 +0000

    add user statuses

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,14 @@ class User extends Authenticatable
     public function sendPasswordResetNotification($token){
         $this->notify(new ResetPassword($token));
     }
+
+    //一个用户多条微博
+    public function statuses(){
+        return $this->hasMany(Status::class);
+    }
+
+
+    public function feed(){
+        return $this->statuses()->orderBy('created_at','desc');
+    }
 }
